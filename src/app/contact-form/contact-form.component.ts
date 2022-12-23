d81bca1ed8d5ac6453b836eb46061637a54d3550
Christoph <c.wagner.germany@gmail.com>
2022-12-23 18:46:31 +0100

    mail junus final

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async sendMail() {
+    console.log('Sending mail', this.myForm);
+    let nameField = this.nameField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+    nameField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+    // Todo: Animation anzeigen
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('message', messageField.value);
+
+    await fetch('https://cwgermany.de/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: fd
+      }
+    );
+
+    // Todo: Text anzeigen: Nachricht gesendet
+    nameField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
   }
 
 }
