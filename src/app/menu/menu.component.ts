75238360b6471a0a81ae9ae03d5988b346cfe984
Christoph <c.wagner.germany@gmail.com>
2023-02-19 13:22:39 +0100

    mobile menu13

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -40,4 +40,12 @@ export class MenuComponent implements OnInit {
     document.getElementById("contact").scrollIntoView();
   }
 
+  toggleMobileNav() {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile__nav');
+
+    menu_btn.classList.toggle('is__active');
+    mobile_menu.classList.toggle('is__active');
+  }
+
 }
